eb5773aa5d5f20c8022f769b3f7afea2194758fb
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2024-01-06 15:19:47 -0600

    hashmap completed

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -39,6 +39,29 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+       // Check if team_1 already exists in the hashmap
+    if let Some(team) = scores.get_mut(&team_1_name) {
+        // If it does, update the team's scores
+        team.goals_scored += team_1_score;
+        team.goals_conceded += team_2_score;
+    } else {
+        // If it doesn't, insert a new team
+        scores.insert(team_1_name, Team {
+            goals_scored: team_1_score,
+            goals_conceded: team_2_score,
+        });
+    }
+
+    // Do the same for team_2
+    if let Some(team) = scores.get_mut(&team_2_name) {
+        team.goals_scored += team_2_score;
+        team.goals_conceded += team_1_score;
+    } else {
+        scores.insert(team_2_name, Team {
+            goals_scored: team_2_score,
+            goals_conceded: team_1_score,
+        });
+    }
     }
     scores
 }
